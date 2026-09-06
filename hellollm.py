ea3666763fa4a59aa5f2a68c321a4139f859cff1
Ritesh Sontakke <sontakkeritesh0@gmail.com>
2026-09-06 21:39:45 +0530

    Model Selection Code

---
+++ hellollm.py
@@ -0,0 +1,26 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+my_api_key = os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("Api key is not found")
+
+client = Groq(api_key=my_api_key)
+
+model = "openai/gpt-oss-20b"
+role = "user"
+prompt = "do you know Roman"
+message={
+     "role": "user",
+    "content": prompt
+
+}
+
+messages = [message]
+
+response = client.chat.completions.create(model= model, messages=messages)
+print(response.choices[0].message.content)
