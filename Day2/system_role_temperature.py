1a65352f3b9ea4f3c409f93363fffd18aaea9fb4
Ritesh Sontakke <sontakkeritesh0@gmail.com>
2026-09-13 01:04:59 +0530

    Add temperature comparison script

--- Day2/system_role_temperature.py
+++ Day2/system_role_temperature.py
@@ -9,3 +9,14 @@ if not my_api_key:
     raise ValueError("Key Not Found plz try again")
 
 client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
+
+response = client.chat.completions.create(
+    model="openai/gpt-oss-20b",
+    temperature = 0.2,
+    messages=[
+        {"role":"system","content":"You are helpful assistant"},
+        {"role":"user","content":"Write Roman in two sentence"}
+    ]
+)
+
+print(response.choices[0].message.content)
